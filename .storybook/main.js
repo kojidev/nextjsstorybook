2a221f0b4fc23fac704c3007e2e42a327b03f487
kojidev <hmu@koji.dev>
2022-02-17 14:10:06 +0300

    reuse config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const { applySvgr } = require('../config/applySvgr');
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -13,15 +15,7 @@ module.exports = {
     "builder": "webpack5"
   },
   webpackFinal: config => {
-    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
-    fileLoaderRule.exclude = /\.svg$/;
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      enforce: 'pre',
-      loader: require.resolve('@svgr/webpack'),
-    });
-
+    applySvgr(config);
     return config;
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
+const { applySvgr } = require('./config/applySvgr');
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
+    applySvgr(config);
 
     return config;
   },
